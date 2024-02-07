18485ba091fb74188316c4fd99be12c87ef6dd61
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-02-07 21:53:25 +0900

    refactor: Define matrix type

--- core/src/matrix.rs
+++ core/src/matrix.rs
@@ -1,4 +1,6 @@
-pub fn multiply(a: [[f64;3];2], b: [[f64;3];2]) -> [[f64;3];2] {
+type Matrix = [[f64;3];2];
+
+pub fn multiply(a: Matrix, b: Matrix) -> Matrix {
     [
         [
             a[0][0]*b[0][0] + a[0][1]*b[1][0] + 0.,
@@ -19,16 +21,16 @@ mod tests {
 
     #[test]
     fn should_be_multiplied_correctly() {
-        const a: [[f64;3];2] = [
+        let A: Matrix = [
             [1., 2., 3.],
             [4., 5., 6.]
         ];
-        const b: [[f64;3];2] = [
+        let B: Matrix = [
             [7., 8., 9.],
             [10., 11., 12.]
         ];
 
-        let result: [[f64;3];2] = multiply(a, b);
+        let result: Matrix = multiply(A, B);
 
         assert_eq!(result, [
            [27., 30., 36.],
